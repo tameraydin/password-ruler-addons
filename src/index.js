ee173d60a2de4c4328dd2818248917caafd92196
Tamer Aydin <tamer.aydin@gmail.com>
2016-03-08 17:25:19 +0100

    initial implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 'use strict'
 
-let passwordRulerAddons = {
+const commonPasswordRules = require('common-password-rules');
+const passwordRulerAddons = {};
 
-};
+Object.keys(commonPasswordRules).forEach((validatorName) => {
+  passwordRulerAddons[validatorName] = () => {
+    let args = arguments.slice();
+    let weight = args.shift();
+
+    return {
+      validate: (password) => {
+        args.unshift(password);
+        return commonPasswordRules[validatorName].apply(args);
+      },
+      weight: weight
+    };
+  };
+});
 
 module.exports = passwordRulerAddons;
