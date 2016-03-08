c68a6f70b2c93c12bbc432f3d32d91f410dda22f
Tamer Aydin <tamer.aydin@gmail.com>
2016-03-08 22:47:42 +0100

    added specs + bugfixes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ const commonPasswordRules = require('common-password-rules');
 const passwordRulerAddons = {};
 
 Object.keys(commonPasswordRules).forEach((validatorName) => {
-  passwordRulerAddons[validatorName] = () => {
-    let args = arguments.slice();
-    let weight = args.shift();
+  passwordRulerAddons[validatorName] = function() {
+    let args = Array.prototype.slice.call(arguments);
+    let validateFn = commonPasswordRules[validatorName];
+    let weight = 1;
+
+    if (args.length === validateFn.length) {
+      weight = args.pop();
+    }
 
     return {
       validate: (password) => {
         args.unshift(password);
-        return commonPasswordRules[validatorName].apply(args);
+        return validateFn.apply(null, args);
       },
       weight: weight
     };
--- test/test.js
+++ test/test.js
@@ -1,3 +1,26 @@
 import test from 'ava';
+import commonPasswordRules from 'common-password-rules';
 import passwordRulerAddons from '../src/index';
 
+test('commonPasswordRules', t => {
+  t.same(Object.keys(commonPasswordRules), Object.keys(passwordRulerAddons));
+
+  t.is(passwordRulerAddons.containsLowerCase().weight, 1);
+  t.is(passwordRulerAddons.containsLowerCase(1, 3).weight, 3);
+
+  t.is(passwordRulerAddons.containsLowerCase().validate('A'),
+    commonPasswordRules.containsLowerCase('A'));
+
+  t.is(passwordRulerAddons.containsLowerCase(2).validate('aA'),
+    commonPasswordRules.containsLowerCase('aA', 2));
+
+  t.is(passwordRulerAddons.containsLowerCase(2, 3).validate('aA'),
+    commonPasswordRules.containsLowerCase('aA', 2));
+
+  t.is(passwordRulerAddons.contains('[x]').validate('x'),
+    commonPasswordRules.contains('x', '[x]'));
+
+  t.is(passwordRulerAddons.contains('[x]', 1, 2).weight, 2);
+  t.is(passwordRulerAddons.contains('[x]').validate('x'),
+    commonPasswordRules.contains('x', '[x]'));
+});
